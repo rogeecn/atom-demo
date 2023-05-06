ff767886cf0fab63ae46fe701cd57c8aa2c7db43
yanghao05 <yanghao05@qianxin.com>
2023-05-06 11:26:49 +0800

    with default provider

--- main.go
+++ main.go
@@ -8,39 +8,15 @@ import (
 	"atom/http/modules/system"
 
 	"github.com/rogeecn/atom"
-	"github.com/rogeecn/atom/container"
 	"github.com/rogeecn/atom/providers/database/sqlite"
-	"github.com/rogeecn/atom/providers/http"
-	"github.com/rogeecn/atom/providers/http/gin"
-	"github.com/rogeecn/atom/providers/log"
 	"github.com/rogeecn/atom/services"
-	"github.com/rogeecn/atom/utils/opt"
 	"github.com/spf13/cobra"
 )
 
-var providers = container.Providers{
-	{
-		Provider: log.Provide,
-		Options: []opt.Option{
-			opt.Prefix(log.DefaultPrefix),
-		},
-	},
-	{
-		Provider: gin.Provide,
-		Options: []opt.Option{
-			opt.Prefix(http.DefaultPrefix),
-		},
-	},
-	{
-		Provider: sqlite.Provide,
-		Options: []opt.Option{
-			opt.Prefix(sqlite.DefaultPrefix),
-		},
-	},
-}
-
 func main() {
-	providers = append(providers, system.Providers()...)
+	providers := atom.DefaultHTTP(sqlite.DefaultProvider()).
+		With(system.Providers())
+
 	opts := []atom.Option{
 		atom.Name("http"),
 		atom.RunE(func(cmd *cobra.Command, args []string) error {
