7316c1afa27254bd7514d2dcf645065b83f6a8d1
yanghao05 <yanghao05@qianxin.com>
2023-06-13 18:04:08 +0800

    modify boot

--- modules/boot/provider.go
+++ modules/boot/provider.go
@@ -1,13 +1,24 @@
 package boot
 
 import (
-	"atom/http/modules/boot/controller"
+	"atom/http/docs"
 
+	"github.com/rogeecn/atom"
+	"github.com/rogeecn/atom-addons/providers/swagger"
 	"github.com/rogeecn/atom/container"
+	"github.com/rogeecn/atom/contracts"
+	"github.com/rogeecn/atom/utils/opt"
 )
 
 func Providers() container.Providers {
 	return container.Providers{
-		{Provider: controller.Provide},
+		{Provider: provideSwagger},
 	}
+}
+
+func provideSwagger(opts ...opt.Option) error {
+	return container.Container.Provide(func(swagger *swagger.Swagger) contracts.Initial {
+		swagger.Load(docs.SwaggerSpec)
+		return nil
+	}, atom.GroupInitial)
 }
